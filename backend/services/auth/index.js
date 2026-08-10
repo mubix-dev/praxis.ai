8ed6c0eef6667a3bbcf7b0b601e13c78e58d1be9
Mubeen Khan <mubix.dev@gmail.com>
2026-08-11 00:31:01 +0500

    feat: implement authentication service with Express, dotenv, and CORS

---
+++ backend/services/auth/index.js
@@ -0,0 +1,33 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import connectDb from "./db/db.js"
+dotenv.config()
+
+const PORT = process.env.PORT || 3001
+
+const app = express()
+
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+    return res.status(200).json({message:"Auth is running!"})
+})
+app.use(cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials :true
+}))
+
+
+connectDb()
+.then(()=>{
+    app.listen(PORT,()=>{
+    console.log(`Auth is running at http://localhost:${PORT}`)
+})
+})
+.catch((err)=>{
+    console.log("MongoDB connection error: ",err)
+    process.exit(1)
+})
+
+
