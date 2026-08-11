f87d6a7676b68ffcd1ceebc635f8540121a2af61
Mubeen Khan <mubix.dev@gmail.com>
2026-08-11 22:45:58 +0500

    feat: add authentication middleware to handle user session validation

---
+++ backend/gateway/middlewares/auth.middleware.js
@@ -0,0 +1,24 @@
+import redis from "../../shared/redis/redis.js"
+
+const isAuth = async(req,res,next)=>{
+    try {
+        const sessionId = req.cookies?.sessionId
+        if(!sessionId){
+            return res.status(400).json({message:"Unauthorized Access!"})
+        }
+
+        const session = await redis.get(`sessionId-${sessionId}`)
+        
+        if(!session){
+            return res.status(400).json({message:"Session Expired!"})
+        }
+
+        req.user = JSON.parse(session)
+
+        next()
+    } catch (error) {
+        return res.status(500).json({message:`isAuth Error: ${error}`})
+    }
+}
+
+export default isAuth
