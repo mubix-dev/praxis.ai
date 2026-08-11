95d8fa9f9ea58ec8c2b28596d9c93496d054a45a
Mubeen Khan <mubix.dev@gmail.com>
2026-08-11 22:47:34 +0500

    feat: add getCurrentUser function to fetch current user data

---
+++ frontend/src/features/getCurrentUser.js
@@ -0,0 +1,12 @@
+import api from "../utils/axios"
+
+const getCurrentUser = async()=>{
+    try {
+        const {data} = await api.get("/api/me")
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default getCurrentUser
