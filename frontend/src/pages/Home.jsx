072b96a61315fb1949327016dd885d5cafd10f3f
Mubeen Khan <mubix.dev@gmail.com>
2026-08-11 23:27:41 +0500

    feat: integrate user authentication by dispatching user data on login

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,12 +3,19 @@ import { signInWithPopup } from "firebase/auth";
 import { auth, googleProvider } from "../utils/firebase";
 import api from "../utils/axios";
 import { FcGoogle } from "react-icons/fc";
+import { useDispatch, useSelector } from "react-redux";
+import { setUserData } from "../redux/userSlice";
 
 function Home() {
+
+  const {userData} = useSelector(state=>state.user)
+  console.log(userData)
+  const dispatch = useDispatch()
+
   const handleLogin = async (token) => {
     try {
       const result = await api.post("/api/auth/login", { token });
-      console.log(result?.data);
+      dispatch(setUserData(result?.data))
     } catch (error) {
       console.log(error);
     }
@@ -24,8 +31,7 @@ function Home() {
   };
   return (
     <div className="h-screen flex bg-[#0d0f14] text-white overflow-hidden ">
-      yuhjhjbn
-      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur-sm">
+      {!userData && <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur-sm">
         <div className="w-85 bg-[#13151c] border border-white/8 rounded-2xl p-7 flex flex-col gap-5">
           <div className="flex flex-col gap-1">
             <h2 className=" text-[17px] font-semibold text-slate-100 tracking-tight">
@@ -40,7 +46,7 @@ function Home() {
             Continue with Google
           </button>
         </div>
-      </div>
+      </div>}
     </div>
   );
 }
