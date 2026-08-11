9a06b303cb39bd8059c1cae0e466d9bff592085d
Mubeen Khan <mubix.dev@gmail.com>
2026-08-11 23:27:19 +0500

    refactor: update getCurrentUser function to return user data instead of logging it

--- frontend/src/features/getCurrentUser.js
+++ frontend/src/features/getCurrentUser.js
@@ -3,7 +3,7 @@ import api from "../utils/axios"
 const getCurrentUser = async()=>{
     try {
         const {data} = await api.get("/api/me")
-        console.log(data)
+        return data
     } catch (error) {
         console.log(error)
     }
