9cb4fea1dbe69cdc9423d782cdf12256a74a8d1a
Mubeen Khan <mubix.dev@gmail.com>
2026-08-12 12:57:45 +0500

    feat: initialize agent service with Express and MongoDB connection

---
+++ backend/services/agent/index.js
@@ -0,0 +1,28 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDb from "./db/db.js"
+dotenv.config()
+
+const PORT = process.env.PORT || 3003
+
+const app = express()
+
+app.use(express.json())
+
+
+app.get("/",(req,res)=>{
+    return res.status(200).json({message:"Agent service is running!"})
+})
+
+connectDb()
+.then(()=>{
+    app.listen(PORT,()=>{
+    console.log(`Agent service is running at http://localhost:${PORT}`)
+})
+})
+.catch((err)=>{
+    console.log("MongoDB connection error: ",err)
+    process.exit(1)
+})
+
+
