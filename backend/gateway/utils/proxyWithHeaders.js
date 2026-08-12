789b68a570f55afbf901e252a007a6bfe144e1e6
Mubeen Khan <mubix.dev@gmail.com>
2026-08-12 13:13:03 +0500

    fix: ensure user ID is added to headers only when user is present in request

--- backend/gateway/utils/proxyWithHeaders.js
+++ backend/gateway/utils/proxyWithHeaders.js
@@ -4,9 +4,10 @@ import proxy from "express-http-proxy"
 export const proxyWithHeaders = (serviceURL)=>{
     return proxy(serviceURL,{
         proxyReqOptDecorator:(proxyReqOpts,srcReq)=>{
-            if(srcReq){
+            if(srcReq.user){
                 proxyReqOpts.headers["x-user-id"] = srcReq.user.userId
             }
+            return proxyReqOpts
         }
     })
 }
