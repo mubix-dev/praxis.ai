579bd210b5f9f7a202c20b52224469e5db04495d
Mubeen Khan <mubix.dev@gmail.com>
2026-08-12 12:49:05 +0500

    feat: create message model with schema for chat messages

---
+++ backend/services/chat/models/message.model.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+const messageSchema = new Schema({
+    conversationId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Conversation"
+    },
+    role:{
+        type:String,
+        enum:["user","assistant"]
+    },
+    content:{
+        type:String
+    }
+},{timestamps:true})
+
+
+const Message = mongoose.model("Message",messageSchema)
+
+export default Message
