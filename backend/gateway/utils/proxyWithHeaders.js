44afe4790c3b44e205728fc2b66cb6935c1fdd4c
Mubeen Khan <mubix.dev@gmail.com>
2026-08-12 12:48:33 +0500

    feat: add proxyWithHeaders utility for service requests

---
+++ backend/gateway/utils/proxyWithHeaders.js
@@ -0,0 +1,12 @@
+import proxy from "express-http-proxy"
+
+
+export const proxyWithHeaders = (serviceURL)=>{
+    return proxy(serviceURL,{
+        proxyReqOptDecorator:(proxyReqOpts,srcReq)=>{
+            if(srcReq){
+                proxyReqOpts.headers["x-user-id"] = srcReq.user.userId
+            }
+        }
+    })
+}
