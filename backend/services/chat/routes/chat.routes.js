14487fde2f4fd2ecdb710171311b59b4849ba00c
Mubeen Khan <mubix.dev@gmail.com>
2026-08-12 12:49:12 +0500

    feat: add chat routes for conversation and message management

---
+++ backend/services/chat/routes/chat.routes.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import { 
+    createConversation, 
+    createMessage, 
+    getALLConversations, 
+    getALLMessages, 
+    updateConversationTitle 
+} from "../controllers/chat.controller.js"; // Adjust path to your controller file
+
+
+const router = express.Router();
+
+router.post('/conversations', createConversation);
+router.get('/conversations', getALLConversations);
+router.patch('/conversations/:conversationId', updateConversationTitle);
+
+router.post('/messages', createMessage);
+router.get('/conversations/:conversationId/messages', getALLMessages); 
+
+export default router;
