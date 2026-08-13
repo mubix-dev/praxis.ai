89928ff4b0651e95538674ed132941cc76c4f7e2
Mubeen Khan <mubix.dev@gmail.com>
2026-08-14 00:57:38 +0500

    feat: enhance Redis connection handling with error logging

--- backend/shared/redis/redis.js
+++ backend/shared/redis/redis.js
@@ -1,9 +1,16 @@
-import {Redis } from "ioredis"
+import dotenv from "dotenv"
+import { Redis } from "ioredis"
+
+dotenv.config()
 
 const redis = new Redis(process.env.REDIS_URL)
 
 redis.on("connect",()=>{
     console.log("Redis Connected!")
+})
+
+redis.on("error",(err)=>{
+    console.log("Redis error:",err.message)
 })
 
 export default redis
