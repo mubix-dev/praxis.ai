5b262f0fdfbcfd1e8be9accbb6851d5eda1e1a11
Mubeen Khan <mubix.dev@gmail.com>
2026-08-14 05:50:22 +0500

    feat: create Artifact component for displaying content

---
+++ frontend/src/components/Artifact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Artifact() {
+  return (
+    <div className="hidden lg:flex flex-col w-64 xl:w-64 h-full shrink-0 border-l border-white/8 overflow-y-auto">
+      Artifact
+    </div>
+  )
+}
+
+export default Artifact
