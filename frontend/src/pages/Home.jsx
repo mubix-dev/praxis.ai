7653ae9059a33d0ea529b2b5df7af6e4235b156b
Mubeen Khan <mubix.dev@gmail.com>
2026-08-14 05:51:04 +0500

    feat: add Sidebar, ChatArea, and Artifact components to Home page layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,11 +5,13 @@ import api from "../utils/axios";
 import { FcGoogle } from "react-icons/fc";
 import { useDispatch, useSelector } from "react-redux";
 import { setUserData } from "../redux/userSlice";
+import Sidebar from "../components/Sidebar";
+import ChatArea from "../components/ChatArea";
+import Artifact from "../components/Artifact";
 
 function Home() {
 
   const {userData} = useSelector(state=>state.user)
-  console.log(userData)
   const dispatch = useDispatch()
 
   const handleLogin = async (token) => {
@@ -31,6 +33,11 @@ function Home() {
   };
   return (
     <div className="h-screen flex bg-[#0d0f14] text-white overflow-hidden ">
+
+      <Sidebar/>
+      <ChatArea/>
+      <Artifact/>
+      
       {!userData && <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur-sm">
         <div className="w-85 bg-[#13151c] border border-white/8 rounded-2xl p-7 flex flex-col gap-5">
           <div className="flex flex-col gap-1">
