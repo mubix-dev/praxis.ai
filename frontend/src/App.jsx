cbce6bab65a4fa25aeedcf042674fc2ecc32e2e6
Mubeen Khan <mubix.dev@gmail.com>
2026-08-14 06:29:54 +0500

    feat: update App component to conditionally render Home or Landing based on user authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import Home from './pages/Home'
+import Landing from './pages/Landing'
 import useGetCurrUser from './hooks/useGetCurrUser'
 import useGetAllConversations from './hooks/useGetAllConversations'
 
 function App() {
   useGetCurrUser()
   useGetAllConversations()
+  const { userData } = useSelector(state => state.user)
   return (
     <>
-      <Home/>
+      {userData ? <Home/> : <Landing/>}
     </>
   )
 }
