3c54dc90c9381011b364861bc89b695ab03f2682
Mubeen Khan <mubix.dev@gmail.com>
2026-08-14 05:50:09 +0500

    feat: add useGetAllConversations hook to App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Home from './pages/Home'
 import useGetCurrUser from './hooks/useGetCurrUser'
+import useGetAllConversations from './hooks/useGetAllConversations'
 
 function App() {
   useGetCurrUser()
+  useGetAllConversations()
   return (
     <>
       <Home/>
