75334724c75d0bb42b10aa1e6496c4164ef53704
Mubeen Khan <mubix.dev@gmail.com>
2026-08-15 06:33:11 +0500

    feat: add images support to createMessage for enhanced message handling

--- backend/services/chat/controllers/chat.controller.js
+++ backend/services/chat/controllers/chat.controller.js
@@ -73,7 +73,7 @@ export const updateConversationTitle = async (req, res) => {
 
 export const createMessage = async (req, res) => {
   try {
-    const { conversationId, role, content } = req.body;
+    const { conversationId, role, content,images } = req.body;
 
     if (!conversationId || !role || !content) {
       return res.status(400).json({ message: "Incomplete Information" });
@@ -93,6 +93,7 @@ export const createMessage = async (req, res) => {
       conversationId,
       role,
       content,
+      images
     });
 
     return res.status(201).json(message);
