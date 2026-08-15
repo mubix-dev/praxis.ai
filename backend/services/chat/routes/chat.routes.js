3617cb0bd6d958b16f597c4d1d941af2bf7f9f68
Mubeen Khan <mubix.dev@gmail.com>
2026-08-15 07:21:08 +0500

    feat: add deleteConversation route to enable conversation deletion

--- backend/services/chat/routes/chat.routes.js
+++ backend/services/chat/routes/chat.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { 
-    createConversation, 
-    createMessage, 
-    getALLConversations, 
-    getALLMessages, 
-    updateConversationTitle 
+import {
+    createConversation,
+    createMessage,
+    getALLConversations,
+    getALLMessages,
+    updateConversationTitle,
+    deleteConversation
 } from "../controllers/chat.controller.js"; // Adjust path to your controller file
 
 
@@ -13,6 +14,7 @@ const router = express.Router();
 router.post('/conversations', createConversation);
 router.get('/conversations', getALLConversations);
 router.patch('/conversations/:conversationId', updateConversationTitle);
+router.delete('/conversations/:conversationId', deleteConversation);
 
 router.post('/messages', createMessage);
 router.get('/conversations/:conversationId/messages', getALLMessages); 
