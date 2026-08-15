39cd70a5ac415a03302816f60999d6e3e1d880be
Mubeen Khan <mubix.dev@gmail.com>
2026-08-15 16:23:57 +0500

    feat: add userLoading state and setUserLoading reducer in userSlice

--- frontend/src/redux/userSlice.js
+++ frontend/src/redux/userSlice.js
@@ -4,15 +4,19 @@ import { createSlice } from "@reduxjs/toolkit";
 const useSlice = createSlice({
     name:"user",
     initialState:{
-        userData:null
+        userData:null,
+        userLoading:true
     },
     reducers:{
         setUserData : (state,action)=>{
             state.userData = action.payload
+        },
+        setUserLoading : (state,action)=>{
+            state.userLoading = action.payload
         }
     }
 })
 
 
-export const {setUserData} = useSlice.actions
+export const {setUserData,setUserLoading} = useSlice.actions
 export default useSlice.reducer
