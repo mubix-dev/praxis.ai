2c2758ad7de0514a71466973cabf9ffcbfb03e22
Mubeen Khan <mubix.dev@gmail.com>
2026-08-15 14:55:05 +0500

    feat: add ChatOpenRouter model and update getllmModel function

--- backend/services/agent/utils/llm.models.js
+++ backend/services/agent/utils/llm.models.js
@@ -1,5 +1,6 @@
 import { ChatGroq } from "@langchain/groq"
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai"
+import { ChatOpenRouter } from "@langchain/openrouter";
 
 const groq = new ChatGroq({
     model: "openai/gpt-oss-120b",
@@ -9,11 +10,20 @@ const gemini = new ChatGoogleGenerativeAI({
     model: "gemini-3.5-flash-lite",
 })
 
+const openrouter = new ChatOpenRouter({
+    model:"deepseek/deepseek-chat",
+    temperature:0,
+    maxTokens:2500
+})
+
 export const getllmModel = (agent)=>{
     switch (agent) {
         case "chat":
             return gemini; 
-
+        case "search":
+            return gemini;
+        case "codeing":
+            return openrouter;
         default:
             return gemini;
     }
