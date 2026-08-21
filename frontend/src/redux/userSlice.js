d08eede242e08a1f1f367a658baea20009c90ebf
Mubeen Khan <mubix.dev@gmail.com>
2026-08-21 17:40:43 +0500

    feat: add credits state and setCredits reducer to user slice

--- frontend/src/redux/userSlice.js
+++ frontend/src/redux/userSlice.js
@@ -5,7 +5,8 @@ const useSlice = createSlice({
     name:"user",
     initialState:{
         userData:null,
-        userLoading:true
+        userLoading:true,
+        credits:0
     },
     reducers:{
         setUserData : (state,action)=>{
@@ -13,10 +14,13 @@ const useSlice = createSlice({
         },
         setUserLoading : (state,action)=>{
             state.userLoading = action.payload
+        },
+        setCredits : (state,action)=>{
+            state.credits = action.payload
         }
     }
 })
 
 
-export const {setUserData,setUserLoading} = useSlice.actions
+export const {setUserData,setUserLoading,setCredits} = useSlice.actions
 export default useSlice.reducer
