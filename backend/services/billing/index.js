ec2eddce8e0a2e68ea24be60b8737c1d059603e6
Mubeen Khan <mubix.dev@gmail.com>
2026-08-21 17:39:13 +0500

    feat: add billing service with Express and MongoDB connection

---
+++ backend/services/billing/index.js
@@ -0,0 +1,31 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDb from "./db/db.js"
+import router from "./routes/credits.routes.js"
+dotenv.config()
+
+const PORT = process.env.PORT || 3004
+
+const app = express()
+
+app.use(express.json())
+
+app.use("/",router)
+
+
+app.get("/",(req,res)=>{
+    return res.status(200).json({message:"Billing service is running!"})
+})
+
+connectDb()
+.then(()=>{
+    app.listen(PORT,()=>{
+    console.log(`Billing service is running at http://localhost:${PORT}`)
+})
+})
+.catch((err)=>{
+    console.log("MongoDB connection error: ",err)
+    process.exit(1)
+})
+
+
