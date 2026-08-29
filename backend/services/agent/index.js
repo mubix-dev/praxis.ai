195a26bebd6a298f997c3b3eae47a802552d5210
Mubeen Khan <mubix.dev@gmail.com>
2026-08-29 23:23:41 +0500

    fix: reorder middleware usage for improved error handling in agent service

--- backend/services/agent/index.js
+++ backend/services/agent/index.js
@@ -7,10 +7,20 @@ const PORT = process.env.PORT || 3003
 const app = express()
 
 app.use(express.json())
-app.use("/",router)
+
 
 app.get("/",(req,res)=>{
     return res.status(200).json({message:"Agent service is running!"})
+})
+
+app.use("/",router)
+
+app.use((err,req,res,next)=>{
+    console.log(err)
+    if(err.status){
+        return res.status(err.status).json(err.data)
+    }
+    return res.status(500).json({ message: "Internal server error" });
 })
 
 connectDb()
