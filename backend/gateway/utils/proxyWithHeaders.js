70963d32dd0068630b69319c50c3c4281116d34a
Mubeen Khan <mubix.dev@gmail.com>
2026-09-02 01:45:18 +0500

    feat: add request size limit to proxyWithHeaders for improved performance

--- backend/gateway/utils/proxyWithHeaders.js
+++ backend/gateway/utils/proxyWithHeaders.js
@@ -3,6 +3,7 @@ import proxy from "express-http-proxy"
 
 export const proxyWithHeaders = (serviceURL)=>{
     return proxy(serviceURL,{
+        limit:"25mb",
         proxyReqOptDecorator:(proxyReqOpts,srcReq)=>{
             if(srcReq.user){
                 proxyReqOpts.headers["x-user-id"] = srcReq.user.userId
